de646f4eeab5b45be3993a9ab86d59466c279040
Alexandre Mutel <alexandre_mutel@live.com>
2013-11-12 23:47:04 +0900

    Update to version 1.1

--- CommonAssemblyInfo.cs
+++ CommonAssemblyInfo.cs
@@ -69,7 +69,7 @@ using System;
 //
 // You can specify all the values or you can default the Revision and Build Numbers
 // by using the '*' as shown below:
-[assembly: AssemblyVersion("1.0.2.*")]
-[assembly: AssemblyFileVersion("1.0.2")]
+[assembly: AssemblyVersion("1.1.0.*")]
+[assembly: AssemblyFileVersion("1.1.0")]
 
 [assembly: CLSCompliant(true)]
