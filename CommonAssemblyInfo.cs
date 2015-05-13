e3d65d7b070febaddc01d87f7c5112e4a9b333b0
Alexandre Mutel <alexandre_mutel@live.com>
2015-05-13 23:54:21 +0900

    Push new nuget 1.4 version

--- CommonAssemblyInfo.cs
+++ CommonAssemblyInfo.cs
@@ -69,7 +69,7 @@ using System;
 //
 // You can specify all the values or you can default the Revision and Build Numbers
 // by using the '*' as shown below:
-[assembly: AssemblyVersion("1.3.0.*")]
-[assembly: AssemblyFileVersion("1.3.0")]
+[assembly: AssemblyVersion("1.4.0.*")]
+[assembly: AssemblyFileVersion("1.4.0")]
 
 [assembly: CLSCompliant(true)]
