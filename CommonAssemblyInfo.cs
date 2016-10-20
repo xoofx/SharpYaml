9aa31a340f4fb360f0d08cc9065679d983e6121a
Alexandre Mutel <alexandre_mutel@live.com>
2016-10-20 08:17:34 +0200

    Bump to 1.5.4

--- CommonAssemblyInfo.cs
+++ CommonAssemblyInfo.cs
@@ -69,7 +69,7 @@ using System;
 //
 // You can specify all the values or you can default the Revision and Build Numbers
 // by using the '*' as shown below:
-[assembly: AssemblyVersion("1.5.3.*")]
-[assembly: AssemblyFileVersion("1.5.3")]
+[assembly: AssemblyVersion("1.5.4.*")]
+[assembly: AssemblyFileVersion("1.5.4")]
 
 [assembly: CLSCompliant(true)]
